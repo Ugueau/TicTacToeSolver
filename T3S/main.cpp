eb3a448d0b1032db92d6e61aab5220dcd7cd404c
Hugo_Millot <97529289+Ugueau@users.noreply.github.com>
2023-09-01 23:14:53 +0200

    update save

--- T3S/main.cpp
+++ T3S/main.cpp
@@ -6,10 +6,10 @@
 int main(void) {
 
 	Match m;
-	Solver s(m.getGrid(), "save0.txt");
-	//Solver s1(m.getGrid(), "save0.txt");
+	Solver s(m.getGrid(), "save3.txt");
+	Solver s1(m.getGrid(), "save4.txt"); //Uncomment for add an AI
 	m.addAI(&s);
-	//m.addAI(&s1);
+	m.addAI(&s1);//Uncomment for add an AI
 	TicTacToeView tttv(&m);
 	int round = 3;
 
@@ -23,7 +23,7 @@ int main(void) {
 		tttv.play();
 		m.reinit();
 		s.reinit(m.getGrid());
-		//s1.reinit(m.getGrid());
+		s1.reinit(m.getGrid());//Uncomment for add an AI
 	}
 	
 	std::cout << round << " round played" << std::endl;
